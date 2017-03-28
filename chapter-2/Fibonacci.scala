c368898254f82bf5697f804560677a86ba029c6b
Eric Hopkins <eric.on.tech@gmail.com>
2017-03-27 23:25:55 -0400

    Improve fibonacci function
    The original fibonacci function that I'd written was overly verbose, so
    I decided to go back and work on it to cut it down to just what it
    needed to be.

--- chapter-2/Fibonacci.scala
+++ chapter-2/Fibonacci.scala
@@ -1,13 +1,10 @@
 object Math {
-  // NOTE: I should come back to this and update it, I'm sure that I can get
-  // tighten this function up.
   def fibonacci(n: Int): Int = {
     @annotation.tailrec
-    def go(i: Int, curr: Int, prev: Int):Int = {
-      if(n == 0 || n == 1) n
-      else if(i == n) curr
-      else go((i + 1), (curr + prev), curr)
+    def go(i: Int, prev: Int, curr: Int): Int = {
+      if(i == 0) prev
+      else go((i - 1), curr, (prev + curr))
     }
-    go(1, 1, 0)
+    go(n, 0, 1)
   }
 }
